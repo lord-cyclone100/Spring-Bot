ce91420bded8de4a62862ebdd2591c08216a64c0
lord-cyclone100 <sang66deb@gmail.com>
2025-09-14 22:20:00 +0530

    added styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import './App.css'
+import './App.scss'
 import axios from 'axios'
 import { BACKEND_URL } from './config'
 import { Navbar } from './components/Navbar'
